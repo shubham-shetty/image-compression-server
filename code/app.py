f946170ed876ffc0d15ff14e154d6289a6bc2119
EC2 Default User <ec2-user@ip-172-31-37-232.ec2.internal>
2021-08-12 04:56:10 +0000

    Additional Flask code

--- code/app.py
+++ code/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 APP_NAME = 'image-compression-server'
 app = Flask(APP_NAME)
 
 @app.route('/')
 def upload_page():
-    return "Hello World"    
+    return "Hello World"
+
+@app.route('/form')
+def form():
+    return render_template('file_submit.html')
+
+@app.route('/upload', methods = ['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return "File saved successfully"
 
 
 
